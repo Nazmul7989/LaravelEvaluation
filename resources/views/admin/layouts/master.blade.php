1a025d5ce3a3536e3f04d62b97d5defb69a67b52
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2022-04-24 17:15:00 +0600

    admin panel set up

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,99 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Mosaddek">
+    <meta name="keyword" content="FlatLab, Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+    <link rel="shortcut icon" href="img/favicon.html">
+
+    <title>FlatLab - Flat & Responsive Bootstrap Admin Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{ asset('admin/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('admin/css/bootstrap-reset.css') }}" rel="stylesheet">
+    <!--external css-->
+    <link href="{{ asset('admin/assets/font-awesome/css/font-awesome.css') }}" rel="stylesheet" />
+    <link href="{{ asset('admin/assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css') }}" rel="stylesheet" type="text/css" media="screen"/>
+    <link rel="stylesheet" href="{{ asset('admin/css/owl.carousel.css') }}" type="text/css">
+
+    <!--right slidebar-->
+    <link href="{{ asset('admin/css/slidebars.css') }}" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+
+    <link href="{{ asset('admin/css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('admin/css/style-responsive.css') }}" rel="stylesheet" />
+
+</head>
+
+<body>
+
+<section id="container">
+    @include('admin.partials.header')
+    @include('admin.partials.sidebar')
+
+
+   @yield('content')
+
+
+    @include('admin.partials.footer')
+
+
+</section>
+
+<!-- js placed at the end of the document so the pages load faster -->
+<script src="{{ asset('admin/js/jquery.js') }} "></script>
+<script src="{{ asset('admin/js/bootstrap.bundle.min.js') }}"></script>
+<script class="include" type="text/javascript" src="{{ asset('admin/js/jquery.dcjqaccordion.2.7.js') }}"></script>
+<script src="{{ asset('admin/js/jquery.scrollTo.min.js') }}"></script>
+<script src="{{ asset('admin/js/jquery.nicescroll.js') }}" type="text/javascript"></script>
+<script src="{{ asset('admin/js/jquery.sparkline.js') }}" type="text/javascript></script>
+<script src="{{ asset('admin/assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js') }}"></script>
+<script src="{{ asset('admin/js/owl.carousel.js') }}"></script>
+<script src="{{ asset('admin/js/jquery.customSelect.min.js') }}" ></script>
+<script src="{{ asset('admin/js/respond.min.js') }}" ></script>
+
+<!--right slidebar-->
+<script src="{{ asset('admin/js/slidebars.min.js') }}"></script>
+
+<!--common script for all pages-->
+<script src="{{ asset('admin/js/common-scripts5e1f.js?v=2') }}"></script>
+
+<!--script for this page-->
+<script src="{{ asset('admin/js/sparkline-chart.js') }}"></script>
+<script src="{{ asset('admin/js/easy-pie-chart.js') }}"></script>
+<script src="{{ asset('admin/js/count.js') }}"></script>
+
+<script>
+
+    //owl carousel
+
+    $(document).ready(function() {
+        $("#owl-demo").owlCarousel({
+            navigation : true,
+            slideSpeed : 300,
+            paginationSpeed : 400,
+            singleItem : true,
+            autoPlay:true
+
+        });
+    });
+
+    //custom select box
+
+    $(function(){
+        $('select.styled').customSelect();
+    });
+
+    $(window).on("resize",function(){
+        var owl = $("#owl-demo").data("owlCarousel");
+        owl.reinit();
+    });
+
+</script>
+
+</body>
+</html>
